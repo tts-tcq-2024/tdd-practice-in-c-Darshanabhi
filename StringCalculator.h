9102f511af8073c377d158144e5a7f8acbcce66b
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-26 22:05:29 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,5 +7,12 @@ int add(const char* numbers) {
   if (numbers == NULL || strlen(numbers) == 0) {
         return 0;
     }
-  return 0;
+    int total = 0;
+    char num_copy = strdup(numbers);
+    char token = strtok(num_copy, ",\n");
+  if (value > 1000) {
+            value = 0;  // Ignore numbers greater than 1000
+        }
+  total = total+ value;
+  return total;
 }
