8d140e05ab159f3e81fae759ac1153a4f4dc1f32
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-26 21:58:31 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+int add(const char* numbers) {
+  if (numbers == NULL || strlen(numbers) == 0) {
+        return 0;
+    }
+  return 0;
+}
